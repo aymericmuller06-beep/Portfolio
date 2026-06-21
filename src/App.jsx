90824936fb218b728fd615f9865bb4240493b543
Aymeric.mu <aymericmuller06@gmail.com>
2026-06-21 16:34:47 +0200

    FIX bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
-import Projects from './pages/Projects'
 import Realisations from './pages/Realisations'
 import TableauSynthese from './pages/TableauSynthese'
 import CV from './pages/CV'
 import Veilletech from './pages/Veilletech'
+import MonEntreprise from './pages/MonEntreprise'
+import Annuaire from './pages/Annuaire'
 
 function App() {
   return (
@@ -15,11 +16,12 @@ function App() {
       <main id="swup">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/pages/projets_stage" element={<Projects />} />
           <Route path="/pages/realisations" element={<Realisations />} />
           <Route path="/pages/tableau_synthese" element={<TableauSynthese />} />
+          <Route path="/pages/mon_entreprise" element={<MonEntreprise />} />
           <Route path="/pages/cv" element={<CV />} />
           <Route path="/pages/veille_technologique" element={<Veilletech />} />
+          <Route path="/pages/annuaire" element={<Annuaire />} />
         </Routes>
       </main>
       <Footer />
